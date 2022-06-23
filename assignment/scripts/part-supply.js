a5b09992cc50dcb1a4ed6b0eb0e100fccda73a4f
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-06-23 13:42:20 -0500

    working on while loop

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -48,10 +48,23 @@ for (let i = 0; i < supplyChanges.length; i++) {
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
-
+for (i in supplyChanges) {
+    if (supplyChanges[i] > 0) {
+        console.log(`Added ${supplyChanges[i]} parts.`)
+    } else if (supplyChanges[i] == 0) {
+        console.log('No Change.')
+    } else if (supplyChanges[i] < 0) {
+        console.log(`Removed ${supplyChanges[i]} parts.`)
+    }
+}
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let totalParts = 0;
+for (i in supplyChanges) {
+    totalParts += supplyChanges[i];
+}
+console.log(totalParts)
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -60,3 +73,13 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+let parts = 572;
+let box = 0;
+let boxes = 0;
+while (parts > 0) {
+    if (box < 7) {
+        box + parts
+    }
+    parts--;
+}
+console.log(box)
